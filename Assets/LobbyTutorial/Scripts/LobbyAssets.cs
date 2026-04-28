8c933567226555c8c0e76aa41c39bb52d1682a4e
Trịnh Thành <101377590+chickenmolin@users.noreply.github.com>
2026-04-28 16:15:57 +0700

    Update

--- Assets/LobbyTutorial/Scripts/LobbyAssets.cs
+++ Assets/LobbyTutorial/Scripts/LobbyAssets.cs
@@ -3,29 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// 1. Công dụng file:
+// - Quản lý asset (Sprite) cho nhân vật trong Lobby
+// - Cung cấp sprite tương ứng với từng loại nhân vật
+
+// 2. Các mục quan trọng:
+
+// a) Dữ liệu chính:
+// - marineSprite: sprite nhân vật Marine
+// - ninjaSprite: sprite nhân vật Ninja
+// - zombieSprite: sprite nhân vật Zombie
+// - Instance: singleton để truy cập toàn cục
+
+// b) Logic chính:
+// - GetSprite(playerCharacter):
+//   + Nhận loại nhân vật
+//   + Trả về sprite tương ứng
+
+// c) Dependencies:
+// - LobbyManager.PlayerCharacter: enum loại nhân vật
+// - Sprite: Unity asset hiển thị hình ảnh
+
+// d) Notes:
+// - Sử dụng Singleton pattern
+// - Dùng switch-case để map enum → sprite
+// - Có default để tránh lỗi khi enum không hợp lệ
+
+// Singleton quản lý sprite cho các nhân vật trong Lobby
+
 public class LobbyAssets : MonoBehaviour {
-
-
-
     public static LobbyAssets Instance { get; private set; }
 
-
+    // Sprite 3 nhân vật, gán trong Inspector
     [SerializeField] private Sprite marineSprite;
     [SerializeField] private Sprite ninjaSprite;
     [SerializeField] private Sprite zombieSprite;
 
+    private void Awake() { Instance = this; }
 
-    private void Awake() {
-        Instance = this;
-    }
-
+    // Trả về sprite tương ứng với nhân vật được chọn
     public Sprite GetSprite(LobbyManager.PlayerCharacter playerCharacter) {
         switch (playerCharacter) {
             default:
-            case LobbyManager.PlayerCharacter.Marine:   return marineSprite;
-            case LobbyManager.PlayerCharacter.Ninja:    return ninjaSprite;
-            case LobbyManager.PlayerCharacter.Zombie:   return zombieSprite;
+            case LobbyManager.PlayerCharacter.Marine:  return marineSprite;
+            case LobbyManager.PlayerCharacter.Ninja:   return ninjaSprite;
+            case LobbyManager.PlayerCharacter.Zombie:  return zombieSprite;
         }
     }
-
 }
