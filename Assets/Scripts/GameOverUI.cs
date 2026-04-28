c52b911de2ede7db8a062aa334f9274fb14fcb9e
Trịnh Thành <101377590+chickenmolin@users.noreply.github.com>
2026-04-28 16:39:39 +0700

    l5

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -2,55 +2,74 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+// 1. Công dụng file:
+// - Quản lý UI hiển thị kết quả game (Win / Lose / Tie)
+// - Cho phép người chơi chơi lại (Rematch)
+
+// 2. Các mục quan trọng:
+
+// a) UI Components:
+// - resultTextMesh: hiển thị kết quả (Win / Lose / Tie)
+// - winColor: màu khi thắng
+// - loseColor: màu khi thua
+// - tieColor: màu khi hòa
+// - rematchButton: nút chơi lại
+
+// b) UI interaction:
+// - Click rematchButton:
+//   + Gửi yêu cầu rematch lên GameManager (RPC)
+
+// c) Event từ GameManager:
+// - OnGameWin:
+//   + Hiển thị "YOU WIN" hoặc "YOU LOSE"
+//   + Đổi màu tương ứng
+// - OnGameTied:
+//   + Hiển thị "TIE"
+//   + Đổi màu tie
+// - OnRematch:
+//   + Ẩn UI
+
+// d) Logic hiển thị:
+// - Show(): hiển thị UI kết quả
+// - Hide(): ẩn UI
+
+// e) Dependencies:
+// - GameManager: cung cấp event và xử lý logic game
+// - TextMeshProUGUI: hiển thị text
+// - Button: xử lý input UI
+
 public class GameOverUI : MonoBehaviour {
-
-
     [SerializeField] private TextMeshProUGUI resultTextMesh;
-    [SerializeField] private Color winColor;
-    [SerializeField] private Color loseColor;
-    [SerializeField] private Color tieColor;
+    [SerializeField] private Color winColor, loseColor, tieColor; // Màu theo kết quả
     [SerializeField] private Button rematchButton;
 
-
     private void Awake() {
-        rematchButton.onClick.AddListener(() => {
-            GameManager.Instance.RematchRpc();
-        });
+        rematchButton.onClick.AddListener(() => GameManager.Instance.RematchRpc()); // Gửi yêu cầu chơi lại lên server
     }
-    private void Start() {
-        GameManager.Instance.OnGameWin += GameManager_OnGameWin;
-        GameManager.Instance.OnRematch += GameManager_OnRematch;
-        GameManager.Instance.OnGameTied += GameManager_OnGameTied;
 
+    private void Start() {
+        GameManager.Instance.OnGameWin  += GameManager_OnGameWin;
+        GameManager.Instance.OnRematch  += GameManager_OnRematch;  // Chơi lại → ẩn UI
+        GameManager.Instance.OnGameTied += GameManager_OnGameTied;
         Hide();
     }
 
     private void GameManager_OnGameTied(object sender, System.EventArgs e) {
-        resultTextMesh.text = "TIE!";
+        resultTextMesh.text  = "TIE!";
         resultTextMesh.color = tieColor;
         Show();
     }
 
-    private void GameManager_OnRematch(object sender, System.EventArgs e) {
-        Hide();
-    }
+    private void GameManager_OnRematch(object sender, System.EventArgs e) => Hide();
 
     private void GameManager_OnGameWin(object sender, GameManager.OnGameWinEventArgs e) {
-        if (e.winPlayerType == GameManager.Instance.GetLocalPlayerType()) {
-            resultTextMesh.text = "YOU WIN!";
-            resultTextMesh.color = winColor;
-        } else {
-            resultTextMesh.text = "YOU LOSE!";
-            resultTextMesh.color = loseColor;
-        }
+        // So sánh người thắng với người chơi local
+        bool isLocalPlayerWin = e.winPlayerType == GameManager.Instance.GetLocalPlayerType();
+        resultTextMesh.text  = isLocalPlayerWin ? "YOU WIN!" : "YOU LOSE!";
+        resultTextMesh.color = isLocalPlayerWin ? winColor   : loseColor;
         Show();
     }
 
-    private void Show() {
-        gameObject.SetActive(true);
-    }
-
-    private void Hide() {
-        gameObject.SetActive(false);
-    }
+    private void Show() { gameObject.SetActive(true); }
+    private void Hide() { gameObject.SetActive(false); }
 }
