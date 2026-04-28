ee74a093b74bdaed58904d6668d8e57cdf8470d7
Trịnh Thành <101377590+chickenmolin@users.noreply.github.com>
2026-04-28 16:11:03 +0700

    Up

--- Assets/LobbyTutorial/Scripts/AuthenticateUI.cs
+++ Assets/LobbyTutorial/Scripts/AuthenticateUI.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// 1. Công dụng file:
+// - Xử lý UI đăng nhập người chơi
+// - Gửi tên người chơi lên hệ thống Lobby
+// - Ẩn UI sau khi authenticate
+
+// 2. Các mục quan trọng:
+
+// a) Dữ liệu chính:
+// - authenticateButton: nút bấm để xác nhận đăng nhập
+
+// b) UI interaction:
+// - Khi click button:
+//   + Lấy tên player từ EditPlayerName
+//   + Gọi LobbyManager để authenticate
+//   + Ẩn UI
+
+// c) Dependencies:
+// - LobbyManager: xử lý kết nối / authenticate
+// - EditPlayerName: lấy tên người chơi nhập vào
+
+// d) Notes:
+// - Sử dụng AddListener để gán sự kiện button
+// - UI sẽ bị disable sau khi login
+
 public class AuthenticateUI : MonoBehaviour {
-
-
-    [SerializeField] private Button authenticateButton;
-
+    [SerializeField] private Button authenticateButton; // Nút đăng nhập (gán trong Inspector)
 
     private void Awake() {
+        // Khi nhấn nút → đăng nhập với tên người chơi và ẩn UI
         authenticateButton.onClick.AddListener(() => {
             LobbyManager.Instance.Authenticate(EditPlayerName.Instance.GetPlayerName());
             Hide();
@@ -17,7 +39,6 @@ public class AuthenticateUI : MonoBehaviour {
     }
 
     private void Hide() {
-        gameObject.SetActive(false);
+        gameObject.SetActive(false); // Ẩn UI đăng nhập
     }
-
 }
